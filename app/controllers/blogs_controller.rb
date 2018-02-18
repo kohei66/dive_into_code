fc0ac623803df11826de2e1ffac4fbd52adc0f59
kohei <hisashi.kk@gmail.com>
2018-02-18 23:29:49 +0900

    bootstrap product

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,59 +1,58 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy,]
 
-  
   def index
-    @blogs=Blog.all
+    @blogs = Blog.all
   end
-  
+
   def new
-    if params[:back]
-      @blog=Blog.new(blog_params)
-    else
-    @blog=Blog.new
-    end
+    @blog = if params[:back]
+              Blog.new(blog_params)
+            else
+              Blog.new
+            end
   end
-  
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
-      redirect_to blogs_path, notice:'ブログを作成しました！'
+      redirect_to blogs_path, notice: 'Clone was successfully created.'
     else
-      render'new'
+      render 'new'
     end
   end
-  
-  def show
+
+  def show; end
+
+  def edit; end
+
+def update
+  if @blog.update(blog_params)
+    redirect_to blogs_path, notice: 'Clone was successfully created.'
+  else
+    render 'edit'
   end
-  
-  def edit
-  end
-  
-  def update
-    if @blog.update(blog_params)
-      redirect_to blogs_path, notice:"ブログを編集しました！"
-    else
-      render 'edit'
-    end
-  end
-  
+end
+
+
   def destroy
     @blog.destroy
-    redirect_to blogs_path, notice:"ブログを削除しました！"
+    redirect_to blogs_path, notice: 'ブログを削除しました！'
   end
-  
+
   def confirm
-    @blog=Blog.new(blog_params)
+    @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
+
   private
+
   def blog_params
-    params.require(:blog).permit(:title,:content)
+    params.require(:blog).permit(:title, :content)
   end
-  
+
   # idをキーとして値を取得するメソッド
   def set_blog
-    @blog=Blog.find(params[:id])
+    @blog = Blog.find(params[:id])
   end
 end
